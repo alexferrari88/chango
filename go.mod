307a8fbd1a2aa93b8101c9175f62b99a08571a47
Alex Ferrari <a.ferrari88@gmail.com>
2022-08-25 12:07:40 +0200

    wip

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/alexferrari88/chango
+
+go 1.19
+
+require (
+	github.com/BurntSushi/toml v1.2.0
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/tidwall/gjson v1.14.3
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,266 @@
+﻿package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/BurntSushi/toml"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/tidwall/gjson"
+)
+
+type Selector struct {
+	Value     string
+	Type      string
+	Threshold string
+	Frequency string
+}
+
+type Subscription struct {
+	Id        string
+	WebsiteId string
+	Threshold string
+	Frequency string
+}
+
+type Website struct {
+	Id           string
+	Selector     Selector
+	Url          string
+	Name         string
+	ScrapingType string
+	JsonKey      string
+	RealBrowser  bool
+}
+
+type Websites []Website
+
+func (ws *Websites) GetById(id string) *Website {
+	for _, w := range *ws {
+		if w.Id == id {
+			return &w
+		}
+	}
+	return &Website{}
+}
+
+type Job struct {
+	Website      *Website
+	Subscription *Subscription
+	Scraper      Scraper
+}
+
+type Result struct {
+	Website      *Website
+	Subscription *Subscription
+	Error        error
+	Value        string
+}
+
+type Scraper interface {
+	Scrape(w *Website) (Result, error)
+}
+
+type JSONScraper struct{}
+
+func (j JSONScraper) Scrape(w *Website) (Result, error) {
+	u := w.Url
+	selector := w.Selector
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		panic(err)
+	}
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return Result{}, err
+	}
+	if res.StatusCode != 200 {
+		return Result{}, fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return Result{}, err
+	}
+	json := string(body)
+	if w.JsonKey != "" {
+		json = gjson.Get(json, w.JsonKey).String()
+	}
+	value := gjson.Get(json, selector.Value)
+	return Result{Value: value.String(), Website: w}, nil
+}
+
+type HTMLScraper struct{}
+
+func (h HTMLScraper) Scrape(w *Website) (Result, error) {
+	u := w.Url
+	selector := w.Selector
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		panic(err)
+	}
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return Result{}, err
+	}
+	if res.StatusCode != 200 {
+		return Result{}, fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return Result{}, err
+	}
+	value := doc.Find(selector.Value).Text()
+	return Result{Value: value, Website: w}, nil
+}
+
+func worker(jobs <-chan Job, results chan<- Result, wg *sync.WaitGroup) {
+	for j := range jobs {
+		result, err := j.Scraper.Scrape(j.Website)
+		if err != nil {
+			results <- Result{Subscription: j.Subscription, Error: err}
+		}
+		results <- Result{Value: result.Value, Subscription: j.Subscription, Website: result.Website}
+	}
+	close(results)
+}
+
+func main() {
+	var websitesParsed map[string]Websites
+	var wg sync.WaitGroup
+	websitesData, err := os.ReadFile("websites.toml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = toml.Decode(string(websitesData), &websitesParsed)
+	if err != nil {
+		log.Fatal(err)
+	}
+	websites := websitesParsed["website"]
+
+	var subsParsed map[string][]Subscription
+	subscriptionsData, err := os.ReadFile("subscriptions.toml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = toml.Decode(string(subscriptionsData), &subsParsed)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jobs := make(chan Job, 100)
+	results := make(chan Result, 100)
+
+	go worker(jobs, results, &wg)
+	go processResults(results, &wg)
+
+	scrapersFactory := map[string]Scraper{"json": JSONScraper{}, "html": HTMLScraper{}}
+
+	subscriptions := subsParsed["subscription"]
+	for _, s := range subscriptions {
+		wg.Add(1)
+		sub := s
+		w := websites.GetById(s.WebsiteId)
+		jobs <- Job{Website: w, Subscription: &sub, Scraper: scrapersFactory[w.ScrapingType]}
+	}
+	close(jobs)
+	wg.Wait()
+}
+
+func processResults(results <-chan Result, wg *sync.WaitGroup) {
+	for result := range results {
+		if result.Error != nil {
+			fmt.Println(result.Error)
+		}
+		fmt.Println(result.Website.Name, result.Value)
+		if result.Subscription.Threshold != "" {
+			if reachedThreshold, err := checkThreshold(result.Subscription.Threshold, result.Value); err != nil {
+				log.Fatal(err)
+			} else if reachedThreshold {
+				fmt.Println("Threshold reached for", result.Website.Name)
+			}
+		}
+		wg.Done()
+	}
+}
+
+func processThresholdString(threshold string) (func(string) (bool, error), error) {
+	s := strings.Split(threshold, " ")
+	if len(s) != 2 {
+		return nil, fmt.Errorf("invalid threshold string")
+	}
+	cmp := s[0]
+	threshValRaw := s[1]
+
+	var threshVal interface{}
+	if threshValRaw == "true" {
+		threshVal = true
+	} else if threshValRaw == "false" {
+		threshVal = false
+	} else if f, err := strconv.ParseFloat(threshValRaw, 64); err == nil {
+		threshVal = f
+	} else {
+		threshVal = threshValRaw
+	}
+	return func(v string) (bool, error) {
+		var vVal interface{}
+		if v == "true" {
+			vVal = true
+		} else if v == "false" {
+			vVal = false
+		} else if f, err := strconv.ParseFloat(v, 64); err == nil {
+			vVal = f
+		} else {
+			vVal = v
+		}
+
+		switch cmp {
+		case "==":
+			return reflect.DeepEqual(vVal, threshVal), nil
+		case "!=":
+			return !reflect.DeepEqual(vVal, threshVal), nil
+		case ">":
+			if reflect.TypeOf(vVal) == reflect.TypeOf(threshVal) {
+				return vVal.(float64) > threshVal.(float64), nil
+			}
+			return false, nil
+		case "<":
+			if reflect.TypeOf(vVal) == reflect.TypeOf(threshVal) {
+				return vVal.(float64) < threshVal.(float64), nil
+			}
+			return false, nil
+		case ">=":
+			if reflect.TypeOf(vVal) == reflect.TypeOf(threshVal) {
+				return vVal.(float64) >= threshVal.(float64), nil
+			}
+			return false, nil
+		case "<=":
+			if reflect.TypeOf(vVal) == reflect.TypeOf(threshVal) {
+				return vVal.(float64) <= threshVal.(float64), nil
+			}
+			return false, nil
+		default:
+			return false, fmt.Errorf("invalid comparison operator")
+		}
+	}, nil
+}
+
+func checkThreshold(threshold string, value string) (bool, error) {
+	if threshOperator, err := processThresholdString(threshold); err != nil {
+		return false, err
+	} else {
+		return threshOperator(value)
+	}
+}
